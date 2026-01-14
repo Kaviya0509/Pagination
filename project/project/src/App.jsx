7d80eb1a6e4805ed767c0345e7707f640016a795
kaviyapriya <kaviyapriya9510.com>
2026-01-14 13:48:36 +0530

    remove space

--- project/project/src/App.jsx
+++ project/project/src/App.jsx
@@ -5,15 +5,12 @@ import "./App.css";
 const App = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const recordsPerPage = 4;
-
   const totalPages = Math.ceil(Data.length / recordsPerPage);
-
   const showingData = () => {
     const firstIndex = (currentPage - 1) * recordsPerPage;
     const lastIndex = firstIndex + recordsPerPage;
     return Data.slice(firstIndex, lastIndex);
   };
-  
 
   const handleNext = () => {
     if (currentPage < totalPages) {
@@ -29,27 +26,22 @@ const App = () => {
 
   return (
     <div className="container">
-
       <h1 className="title">E-Commerce Products</h1>
-
       {/* FLEX IMAGE GRID */}
       <div className="product-grid">
         {showingData().map((value,index) => (
           <div key={value.id} className="product-card">
-            
             <img 
               src={value.imageUrl}
               alt={value.name}
               className="product-img"
             />
-
             <h3 className="product-name">{value.name}</h3>
             <p className="product-info">{value.category}</p>
             <p className="product-price">${value.price}</p>
           </div>
         ))}
       </div>
-
       {/* Pagination */}
       <div className="pagination">
         <button 
@@ -59,11 +51,9 @@ const App = () => {
         >
           Previous
         </button>
-
         <span className="page-text">
           Page {currentPage} of {totalPages}
         </span>
-
         <button 
           className="btn" 
           onClick={handleNext} 
